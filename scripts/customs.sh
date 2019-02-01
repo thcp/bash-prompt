0510d06410c50317ebe8abacdc7cf8aae4ddc308
Thales Pereira <31625914+thcp@users.noreply.github.com>
2019-02-01 10:40:19 +0000

    Update customs.sh

--- scripts/customs.sh
+++ scripts/customs.sh
@@ -2,8 +2,8 @@
 
 function custom_prompt()
 {
-  TFWS=$(command terraform workspace show 2>/dev/null | grep "^*")
-  if [ -n "$TFWS" ]; then
+  TFWS=$(command terraform workspace show 2>/dev/null)
+  if [[ -n "$TFWS" && -d ".terraform" ]]; then
     printf "\ntfws  : ${TFWS}"
   fi
   BRANCH="$(command git branch 2>/dev/null | grep "^*" | colrm 1 2)"
